321c8ef80ac3fe606489ed7664d6757e68af1e60
vincent-gaines <vincentanggaines@gmail.com>
2025-05-21 11:42:56 -0400

    more comments

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3000;
 
 // set up github actions on  a push to main branch
 // so I had to set the CI variable in the actions file
+// again
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
